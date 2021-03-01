6dbeaeb110016719b20d75b23a65b15d72ed36d3
Stephen <stephen.fuhrmann@outlook.de>
2021-03-01 10:31:49 +0100

    GenerateAccessToken: Add TMap with Reference to Bound Delegate to make sure the correct Client will process the response
    This is necessary since the api does not return the UserId with the accesstoken response

--- Source/ScillSDK/Private/ScillBlueprintClasses/ScillClientBackend.cpp
+++ Source/ScillSDK/Private/ScillBlueprintClasses/ScillClientBackend.cpp
@@ -4,17 +4,21 @@ void UScillClientBackend::SetApiKey(FString apiKey)
 {
 	this->authApi.AddHeaderParam("Authorization", "Bearer " + apiKey);
 	this->ApiKey = apiKey;
-	UE_LOG(LogScillSDK, Log, TEXT("API Key Created: %s"), *apiKey);
+	//UE_LOG(LogScillSDK, Log, TEXT("API Key Created: %s"), *apiKey);
 }
 
-void UScillClientBackend::GenerateAccessToken(FString userId)
+void UScillClientBackend::GenerateAccessToken(FString userId, const FReceiveAccessToken& OnAccessTokenGenerated)
 {
 	auto accessTokenRequest = ScillSDK::ScillApiAuthApi::GenerateAccessTokenRequest();
 
 	accessTokenRequest.ScillApiForeignUserIdentifier = ScillSDK::ScillApiForeignUserIdentifier();
 	accessTokenRequest.ScillApiForeignUserIdentifier.UserId = userId;
 
-	auto generateAccessTokenDelegate = ScillSDK::ScillApiAuthApi::FGenerateAccessTokenDelegate::CreateUObject(this, &UScillClientBackend::ReceiveAccessTokenResponse);
+	FGuid guid = FGuid::NewGuid();
+
+	callbackMapReceiveAccessToken.Add(guid, OnAccessTokenGenerated);
+
+	auto generateAccessTokenDelegate = ScillSDK::ScillApiAuthApi::FGenerateAccessTokenDelegate::CreateUObject(this, &UScillClientBackend::ReceiveAccessTokenResponse, guid);
 
 	authApi.GenerateAccessToken(accessTokenRequest, generateAccessTokenDelegate);
 }
@@ -24,13 +28,14 @@ void UScillClientBackend::BeginPlay()
 	Super::BeginPlay();
 
 	this->authApi.AddHeaderParam("Authorization", "Bearer " + this->ApiKey);
-	UE_LOG(LogScillSDK, Log, TEXT("API Key Created: %s"), *this->ApiKey);
+	//UE_LOG(LogScillSDK, Log, TEXT("API Key Created: %s"), *this->ApiKey);
 }
 
-void UScillClientBackend::ReceiveAccessTokenResponse(const ScillSDK::ScillApiAuthApi::GenerateAccessTokenResponse& Response)
+void UScillClientBackend::ReceiveAccessTokenResponse(const ScillSDK::ScillApiAuthApi::GenerateAccessTokenResponse& Response, FGuid guid) const
 {
 	bool isSuccessful = Response.IsSuccessful();
 	FString token = isSuccessful ? Response.Content.Token.GetValue() : "";
 
-	AccessTokenGenerated.Broadcast(token, isSuccessful);
+	auto callback = callbackMapReceiveAccessToken.FindRef(guid);
+	callback.ExecuteIfBound(token, isSuccessful);
 }
--- Source/ScillSDK/Public/ScillBlueprintClasses/ScillClientBackend.h
+++ Source/ScillSDK/Public/ScillBlueprintClasses/ScillClientBackend.h
@@ -1,21 +1,25 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "UObject/Object.h"
+#include "Containers/Map.h"
 #include "ScillSDK.h"
 #include "ScillApiWrapper/ScillApiAuthApi.h"
 #include "ScillApiWrapper/ScillApiAuthApiOperations.h"
+#include "Misc/Guid.h"
 #include "ScillClientBackend.generated.h"
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FAccessTokenGenerated, FString, Token, bool, Success);
+DECLARE_DYNAMIC_DELEGATE_TwoParams(FReceiveAccessToken, FString, Token, bool, Success);
 
 /* This is the entry component for your game's server logic in regards of the SCILL API. This component uses the API Key of your App and should not be created on the client - so best place it on your GameMode class. */
-
 UCLASS(meta = (BlueprintSpawnableComponent))
 class SCILLSDK_API UScillClientBackend : public UActorComponent
 {
+
 	GENERATED_BODY()
 public:
+
+	// SETUP
+
 	/* This changes the set API Key for your app. A good practice is to read the API Key from a configuration file that is not accessible to your Client. */
 	UFUNCTION(BlueprintCallable)
 		void SetApiKey(FString apiKey);
@@ -24,19 +28,25 @@ public:
 	UPROPERTY(EditDefaultsOnly)
 		FString ApiKey;
 
+	virtual void BeginPlay() override;
+
+	// Generate Access Token
+
 	/* Requests an access token that you can then pass to your client. The client can the authenticate itself to your SCILL APP as the user with the passed user id. */
 	UFUNCTION(BlueprintCallable)
-		void GenerateAccessToken(FString userId);
+		void GenerateAccessToken(FString userId, const FReceiveAccessToken& OnAccessTokenGenerated);
 
-	/* Delegate that is called once the Access Token was generated. Assign an Event or Function to it to pass the access token to the respective Client. */
-	UPROPERTY(BlueprintAssignable)
-		FAccessTokenGenerated AccessTokenGenerated;
+	// Start Monitor Battlepass Updates
 
-	virtual void BeginPlay() override;
+	/* Starts to monitor Battlepass updates. */
+	/*UFUNCTION(BlueprintCallable)
+		void StartMonitorBattlePassUpdates(FString accessToken);*/
 
 private:
 	ScillSDK::ScillApiAuthApi authApi;
 
-	void ReceiveAccessTokenResponse(const ScillSDK::ScillApiAuthApi::GenerateAccessTokenResponse& Response);
+	TMap<FGuid, FReceiveAccessToken> callbackMapReceiveAccessToken;
+
+	void ReceiveAccessTokenResponse(const ScillSDK::ScillApiAuthApi::GenerateAccessTokenResponse& Response, FGuid guid) const;
 };
 
