1ce5460975bb320df26a5fd9776c2f65aae31e57
Stephen <stephen.fuhrmann@outlook.de>
2021-03-07 22:30:03 +0100

    finished writing the "FromBattlePass" function on ustruct battle pass

--- Source/ScillSDK/Private/ScillBlueprintClasses/ScillStructs.cpp
+++ Source/ScillSDK/Private/ScillBlueprintClasses/ScillStructs.cpp
@@ -10,9 +10,25 @@ FBattlePass FBattlePass::FromScillApiBattlePass(ScillSDK::ScillApiBattlePass bat
 	bp.AppId = battlePass.AppId.Get("");
 	bp.BattlePassDescription = battlePass.BattlePassDescription.Get("");
 	bp.BattlePassDisclaimer = battlePass.BattlePassDisclaimer.Get("");
-
-	// TODO:
-	// Finish writing this function
+	bp.BattlePassId = battlePass.BattlePassId.Get("");
+	bp.BattlePassName = battlePass.BattlePassName.Get("");
+	bp.BattlePassPriority = battlePass.BattlePassPriority.Get(0);
+	bp.BattlePassShortDescription = battlePass.BattlePassDescription.Get("");
+	bp.CanPurchaseWithCoins = battlePass.CanPurchaseWithCoins.Get(false);
+	bp.CanPurchaseWithMoney = battlePass.CanPurchaseWithMoney.Get("");
+	bp.EndDate = battlePass.EndDate.Get("");
+	bp.ImageL = battlePass.ImageL.Get("");
+	bp.ImageM = battlePass.ImageM.Get("");
+	bp.ImageS = battlePass.ImageS.Get("");
+	bp.ImageXl = battlePass.ImageXl.Get("");
+	bp.ImageXs = battlePass.ImageXs.Get("");
+	bp.IsActive = battlePass.IsActive.Get(false);
+	bp.IsUnlockedIncrementally = battlePass.IsUnlockedIncrementally.Get("");
+	bp.PackageSkuAndroid = battlePass.PackageSkuAndroid.Get("");
+	bp.PackageSkuIos = battlePass.PackageSkuIos.Get("");
+	bp.ReadMoreLink = battlePass.ReadMoreLink.Get("");
+	bp.StartDate = battlePass.StartDate.Get("");
+	bp.UnlockedAt = battlePass.UnlockedAt.Get("");
 
 	return bp;
 }
