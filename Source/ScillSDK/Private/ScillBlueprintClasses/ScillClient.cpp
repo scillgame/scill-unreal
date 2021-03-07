64a42d5269f6bd73c1b3c17ad5c3f56b806e1389
Stephen <stephen.fuhrmann@outlook.de>
2021-03-07 22:10:39 +0100

    start battle pass api
    - also add persistence interface for users to implement in their game instance class

---
+++ Source/ScillSDK/Private/ScillBlueprintClasses/ScillClient.cpp
@@ -0,0 +1,153 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScillBlueprintClasses/ScillClient.h"
+
+// Sets default values for this component's properties
+UScillClient::UScillClient()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+void UScillClient::SetAccessToken(FString newAccessToken)
+{
+	this->battlePassesApi.AddHeaderParam("Authorization", "Bearer " + newAccessToken);
+	this->AccessToken = newAccessToken;
+	
+	auto gameInstance = UGameplayStatics::GetGameInstance(GetWorld());
+	if (gameInstance->Implements<UScillLevelPersistenceInterface>())
+	{
+		IScillLevelPersistenceInterface::Execute_SetAccessToken(gameInstance, newAccessToken);
+	}
+}
+
+FString UScillClient::GetAccessToken() const
+{
+	auto value = FString();
+
+	auto gameInstance = UGameplayStatics::GetGameInstance(GetWorld());
+	if (gameInstance->Implements<UScillLevelPersistenceInterface>())
+	{
+		IScillLevelPersistenceInterface::Execute_GetAccessToken(gameInstance, value);
+	}
+
+	return value;
+}
+
+FString UScillClient::GetUserId() const
+{
+	auto value = FString();
+
+	auto gameInstance = UGameplayStatics::GetGameInstance(GetWorld());
+	if (gameInstance->Implements<UScillLevelPersistenceInterface>())
+	{
+		IScillLevelPersistenceInterface::Execute_GetUserId(gameInstance, value);
+	}
+
+	return value;
+}
+
+void UScillClient::SetUserId(FString newUserId)
+{
+	this->UserId = newUserId;
+
+	auto gameInstance = UGameplayStatics::GetGameInstance(GetWorld());
+	if (gameInstance->Implements<UScillLevelPersistenceInterface>())
+	{
+		IScillLevelPersistenceInterface::Execute_SetUserId(gameInstance, newUserId);
+	}
+}
+
+void UScillClient::ActivateBattlePassLevel(FString levelId, FHttpResponseReceived responseReceived)
+{
+	auto activateBattlePassRequest = ScillSDK::ScillApiBattlePassesApi::ActivateBattlePassLevelRequest();
+
+	activateBattlePassRequest.AppId = AppId;
+	activateBattlePassRequest.LevelId = levelId;
+
+	FGuid guid = FGuid::NewGuid();
+
+	callbackMapResponseReceived.Add(guid, responseReceived);
+
+	auto activateBattlePassRequestDelegate = ScillSDK::ScillApiBattlePassesApi::FActivateBattlePassLevelDelegate::CreateUObject(this, &UScillClient::ReceiveActivateBattlePassLevelResponse, guid);
+	
+	battlePassesApi.ActivateBattlePassLevel(activateBattlePassRequest, activateBattlePassRequestDelegate);
+}
+
+void UScillClient::ClaimBattlePassLevel(FString levelId, FHttpResponseReceived responseReceived)
+{
+	auto request = ScillSDK::ScillApiBattlePassesApi::ClaimBattlePassLevelRewardRequest();
+
+	request.AppId = AppId;
+	request.LevelId = levelId;
+
+	FGuid guid = FGuid::NewGuid();
+
+	callbackMapResponseReceived.Add(guid, responseReceived);
+
+	auto delegate = ScillSDK::ScillApiBattlePassesApi::FClaimBattlePassLevelRewardDelegate::CreateUObject(this, &UScillClient::ReceiveClaimBattlePassLevelResponse, guid);
+
+	battlePassesApi.ClaimBattlePassLevelReward(request, delegate);
+}
+
+// Called when the game starts
+void UScillClient::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto accessToken = FString();
+	auto userId = FString();
+
+	auto gameInstance = UGameplayStatics::GetGameInstance(GetWorld());
+	if (gameInstance->Implements<UScillLevelPersistenceInterface>())
+	{
+		IScillLevelPersistenceInterface::Execute_GetAccessToken(gameInstance, accessToken);
+		IScillLevelPersistenceInterface::Execute_GetUserId(gameInstance, userId);
+	}
+
+	// Set access token to value of level persistence
+	if (!accessToken.IsEmpty())
+		this->AccessToken = accessToken;
+
+	if (!userId.IsEmpty())
+		this->UserId = userId;
+
+	SetAccessToken(this->AccessToken);
+	SetUserId(this->UserId);
+
+	this->battlePassesApi.AddHeaderParam("Authorization", "Bearer " + this->AccessToken);
+
+	// ...
+	
+}
+
+void UScillClient::ReceiveActivateBattlePassLevelResponse(const ScillSDK::ScillApiBattlePassesApi::ActivateBattlePassLevelResponse& Response, FGuid guid) const
+{
+	auto callback = callbackMapResponseReceived.FindRef(guid);
+	callback.ExecuteIfBound(Response.IsSuccessful());
+
+	callbackMapResponseReceived.Remove(guid);
+}
+
+void UScillClient::ReceiveClaimBattlePassLevelResponse(const ScillSDK::ScillApiBattlePassesApi::ClaimBattlePassLevelRewardResponse& Response, FGuid guid) const
+{
+	auto callback = callbackMapResponseReceived.FindRef(guid);
+	callback.ExecuteIfBound(Response.IsSuccessful());
+
+	callbackMapResponseReceived.Remove(guid);
+}
+
+
+// Called every frame
+void UScillClient::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
---
+++ Source/ScillSDK/Public/ScillBlueprintClasses/ScillClient.h
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "ScillApiWrapper/ScillApiBattlePassesApi.h"
+#include "ScillApiWrapper/ScillApiBattlePassesApiOperations.h"
+#include "ScillBlueprintClasses/ScillStructs.h"
+#include "ScillBlueprintClasses/ScillLevelPersistenceInterface.h"
+#include "Kismet/GameplayStatics.h"
+#include "ScillClient.generated.h"
+
+DECLARE_DYNAMIC_DELEGATE_OneParam(FHttpResponseReceived, bool, Success);
+
+
+UCLASS(ClassGroup=(ScillSDK), meta=(BlueprintSpawnableComponent) )
+class SCILLSDK_API UScillClient : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UScillClient();
+
+	// ----------------------------------------------------
+	// Access Token
+
+	/* Sets the access token - normally retreived by the GenerateAccessToken function of the ScillClientBackend-Component. */
+	UFUNCTION(BlueprintCallable, meta=(BlueprintInternalUseOnly))
+		void SetAccessToken(FString newAccessToken);
+
+	/* You can set an access token for testing purposes here, although it is better received by the GenerateAccessToken function of the ScilLClientBackend-Component at runtime. */
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter=GetAccessToken, BlueprintSetter=SetAccessToken)
+		FString AccessToken;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly))
+		FString GetAccessToken() const;
+
+	// ----------------------------------------------------
+	// User Id
+
+	/* You can set a user id for testing purposes here. If set during runtime use the SetUserId function. */
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = GetUserId, BlueprintSetter = SetUserId)
+		FString UserId;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly))
+		FString GetUserId() const;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly))
+		void SetUserId(FString newUserId);
+
+	// ----------------------------------------------------
+	// App Id
+
+	/* The Id of your App. This can be publicly shared, so you can safely put it in this field and deploy it within your client application. */
+	UPROPERTY(EditDefaultsOnly)
+		FString AppId;
+
+	// ----------------------------------------------------
+	// Battle Passes
+
+	UFUNCTION(BlueprintCallable)
+		void ActivateBattlePassLevel(FString levelId, FHttpResponseReceived responseReceived);
+
+	UFUNCTION(BlueprintCallable)
+		void ClaimBattlePassLevel(FString levelId, FHttpResponseReceived responseReceived);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:
+	ScillSDK::ScillApiBattlePassesApi battlePassesApi;
+
+	void ReceiveActivateBattlePassLevelResponse(const ScillSDK::ScillApiBattlePassesApi::ActivateBattlePassLevelResponse& Response, FGuid guid) const;
+	void ReceiveClaimBattlePassLevelResponse(const ScillSDK::ScillApiBattlePassesApi::ClaimBattlePassLevelRewardResponse& Response, FGuid guid) const;
+
+	// ----------------------------------------------------------------------------------
+	// Battle Passes Helpers
+
+	mutable TMap<FGuid, FHttpResponseReceived> callbackMapResponseReceived;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+		
+};
